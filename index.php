4838fd5db7b34f7adeebd2e489df7ee12dd322f4
Jason Jho <jasonjho@gmail.com>
2013-02-24 08:10:48 -0500

    updates

--- index.php
+++ index.php
@@ -15,10 +15,11 @@ include("common.php");
     <div data-role="page" id="scorecard">  
         <!-- ====== main content starts here ===== -->  
         <div data-role="header" id="hdrMain" name="hdrMain" data-nobackbtn="true">
-            <div>
+
                 <h1>NYUAD 2013 <?php if(isset($judge) && $judge): ?>Judge <?php endif; ?></h1>
-                <h2><?php print($email); ?></h2>
-            </div>
+                <div>
+                    <h2><?php print($email); ?></h2>
+                </div>
         </div>  
         <div data-role="content" id="contentMain" name="contentMain">  
 	<?php if(isset($flash_message) && $flash_message): ?>
