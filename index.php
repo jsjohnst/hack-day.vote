d8196902b0bdb1e81f35e95c71a4b38bda06ff43
Jason Jho <jasonjho@gmail.com>
2013-02-24 08:27:54 -0500

    updates

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ include("common.php");
     <div data-role="page" id="scorecard">  
         <!-- ====== main content starts here ===== -->  
         <div data-role="header" id="hdrMain" name="hdrMain" data-nobackbtn="true">
-            <h1 style="text-align:center;">NYUAD 2013 <?php if(isset($judge) && $judge): ?>Judge <?php endif; ?></h1>
+            <div><h1 style="text-align:center;">NYUAD 2013 <?php if(isset($judge) && $judge): ?>Judge <?php endif; ?></h1></div>
             <h2><?php print($email); ?></h2>
         </div>  
         <div data-role="content" id="contentMain" name="contentMain">  
