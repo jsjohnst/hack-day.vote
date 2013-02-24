e1fa1b792ee49b8bd9dd54deffaf26ec4a448a37
Jason Jho <jasonjho@gmail.com>
2013-02-24 06:26:16 -0500

    updates

--- results.php
+++ results.php
@@ -41,11 +41,6 @@ ksort($audience);
 <head>
     <title>Results</title>
     <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.0/css/bootstrap-combined.min.css" rel="stylesheet">
-    <style>
-      body {
-        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-      }
-    </style>
 </head>
 <body>
     <?php foreach($who as $var => $label): ?>
